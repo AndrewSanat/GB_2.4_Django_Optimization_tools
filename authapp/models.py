d5c20844ccfebee5cd6bb777c607cc6457d3da2b
sorokinandan <sorokinandan@gmail.com>
2021-08-06 21:18:49 +0300

    2. Реализована связь моделей «один-к-одному», обеспечить возможность редактирования профиля.

--- authapp/models.py
+++ authapp/models.py
@@ -5,6 +5,9 @@ from django.conf import settings
 import pytz
 from datetime import datetime, timedelta
 
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 class ShopUser(AbstractUser):
     avatar = models.ImageField(upload_to='users_avatars', blank=True)
@@ -17,3 +20,25 @@ class ShopUser(AbstractUser):
         if datetime.now(pytz.timezone(settings.TIME_ZONE)) < (self.activation_key_created + timedelta(hours=48)):
             return False
         return True
+
+
+class ShopUserProfile(models.Model):
+    MALE = 'M'
+    FEMALE = 'W'
+    GENDER_CHOICES = (
+        (MALE, 'М'),
+        (FEMALE, 'Ж'),
+    )
+    user = models.OneToOneField(ShopUser, on_delete=models.CASCADE, unique=True, db_index=True)
+    tagline = models.CharField(max_length=150, blank=True, verbose_name='тэги')
+    about_me = models.TextField(blank=True, verbose_name='о себе')
+    gender = models.CharField(choices=GENDER_CHOICES, blank=True, max_length=1, verbose_name='пол')
+
+    @receiver(post_save, sender=ShopUser)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            ShopUserProfile.objects.create(user=instance)
+
+    @receiver(post_save, sender=ShopUser)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.shopuserprofile.save()
