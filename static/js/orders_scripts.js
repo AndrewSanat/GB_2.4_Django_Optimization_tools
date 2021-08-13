de271931c1e1a54c76aea28021a80adeed0340b0
sorokinandan <sorokinandan@gmail.com>
2021-08-14 00:47:36 +0300

    3. Расширен функционал работы с формами при помощи «django-dynamic-formset».

--- static/js/orders_scripts.js
+++ static/js/orders_scripts.js
@@ -54,4 +54,18 @@ window.onload = function () {
         $('.order_total_cost').html(order_total_cost);
     }
 
+    $('.formset_row').formset({
+       addText: 'добавить продукт',
+       deleteText: 'удалить',
+       prefix: 'orderitems',
+       removed: deleteOrderItem
+    });
+
+    function deleteOrderItem(row) {
+        var target_name= row[0].querySelector('input[type="number"]').name;
+        orderitem_num = parseInt(target_name.replace('orderitems-', '').replace('-quantity', ''));
+        delta_quantity = -quantity_arr[orderitem_num];
+        orderSummaryUpdate(price_arr[orderitem_num], delta_quantity);
+    }
+
 }
